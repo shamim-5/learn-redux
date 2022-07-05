e0505655f3ebeeceea58e184f0adf4c22c531634
mss38 <mss38k@gmail.com>
2022-07-05 13:08:41 +0600

    start learn redux

---
+++ index.js
@@ -0,0 +1,87 @@
+const { createStore } = require("redux");
+
+//CONSTANTS
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+const INCREMENT_COUNTER_BY_VALUE = "INCREMENT_COUNTER_BY_VALUE";
+
+// state
+const initialState = {
+  count: 0,
+};
+
+// action
+
+const incrementCounterAction = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+const decrementCounterAction = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+const resetCounterAction = () => {
+  return {
+    type: RESET,
+  };
+};
+const incrementCounterByValue = (value) => {
+  return {
+    type: INCREMENT_COUNTER_BY_VALUE,
+    payload: value,
+  };
+};
+
+// reducer
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    case RESET:
+      return {
+        ...state,
+        count: 0,
+      };
+    case INCREMENT_COUNTER_BY_VALUE:
+      return {
+        ...state,
+        count: state.count + action.payload,
+      };
+
+    default:
+      state;
+  }
+};
+
+// store
+const store = createStore(counterReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// store.dispatch(incrementCounterAction());
+// store.dispatch(incrementCounterAction());
+// store.dispatch(incrementCounterAction());
+// store.dispatch(resetCounterAction());
+// store.dispatch(decrementCounterAction());
+
+store.dispatch(incrementCounterByValue(5));
+store.dispatch(resetCounterAction());
+store.dispatch(incrementCounterByValue(15));
+
+
