f2385c7cdb90a8fffd458f984c4eb8ba92346a2a
mss38 <mss38k@gmail.com>
2022-07-18 13:21:52 +0600

    fetch data using axios and redux-thunk

--- src/__steps.js
+++ src/__steps.js
@@ -5,5 +5,18 @@
  * 4. store
  * 5. providing store in react
  * 6. use store
- * 
  */
+
+// react-redux fetch data
+/**
+ * step-1: install packages
+ * step-2: constant define
+ * step-3: async action creator => redux-thunk
+ * step-4: reducer
+ * step-5: create store
+ * step-6: provide store
+ * step-7: use store
+ * step-8: adding css
+ */
+
+// npm install redux react-redux axios redux-thunk
