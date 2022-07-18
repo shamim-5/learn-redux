7481ca9ed46f61fbc8a6fcbbb9fecc5116b349eb
mss38 <mss38k@gmail.com>
2022-07-18 13:25:36 +0600

    blank

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Redux</h1>
-      <Counter />
     </div>
   );
 }
