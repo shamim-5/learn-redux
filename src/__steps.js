de954028f33a538dd7f1ad0ec719b3c177de5ccc
mss38 <mss38k@gmail.com>
2022-07-18 09:46:20 +0600

    start apply redux on react

---
+++ src/__steps.js
@@ -0,0 +1,9 @@
+/**
+ * 1. state - count: 0
+ * 2. actions - increment, decrement, reset
+ * 3. reducers - increment --> count => count + 1
+ * 4. store
+ * 5. providing store in react
+ * 6. use store
+ * 
+ */
