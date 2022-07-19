42c48f54874d14b670cc9bb207ddb1ece40309d7
mss38 <mss38k@gmail.com>
2022-07-19 12:08:26 +0600

    fetch data using redux-toolkit on react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import CounterView from './features/counter/CounterView';
+import PostsView from './features/posts/PostsView';
 
 function App() {
   return (
     <div className="App">
       <h3>Hello @reduxjs/toolkit</h3>
       <CounterView />
+      <PostsView />
     </div>
   );
 }
